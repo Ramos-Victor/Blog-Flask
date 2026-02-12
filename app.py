9efbe5897d0b319b93379ebbcac3fe2c978bbb77
Ramos-Victor <vm02oliveiraramos@gmail.com>
2026-02-11 22:50:45 -0300

    feat(blog): Adicionado função de publicar um artigo

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from forms import RegistrarArtigo
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '12345'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///artigos.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -18,7 +20,8 @@ class Artigos(db.Model):
 def __repr__(self):
     return f'<Artigos {self.id}>'
 
-db.create_all()
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def home():
@@ -38,6 +41,27 @@ def admin_dashboard():
                             brand_name= 'Victor Blog',
                             now_year= datetime.now().year,
                             artigos=artigos)
+
+@app.route('/novo_artigo', methods=['GET','POST'])
+def novo_artigo():
+    form = RegistrarArtigo()
+    if form.validate_on_submit():
+        novo = Artigos(
+            titulo=form.titulo.data,
+            conteudo=form.conteudo.data,
+            publicado_em=datetime.now()
+        )
+        db.session.add(novo)
+        db.session.commit()
+        return redirect(url_for('admin_dashboard'))
+    
+    return render_template(
+        'admin/new.html',
+        titulo='NovoArtigo',
+        brand_name='Victor Blog',
+        now_year=datetime.now().year,
+        form=form
+    )
 
 @app.route('/artigo/<int:artigo_id>')
 def artigo(artigo_id):
--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 class RegistrarArtigo(FlaskForm):
-    titulo = StringField('titulo', validators=[DataRequired()])
-    conteudo = StringField('titulo', validators=[DataRequired()])
-    enviar = SubmitField('Submit')
+    titulo = StringField('Título do artigo', validators=[DataRequired()])
+    conteudo = TextAreaField('Conteudo do artigo', validators=[DataRequired()])
+    enviar = SubmitField('Publicar')
